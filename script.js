0e09b8d9f5d445065be339f32c3f122cd456ade0
maezawa-lab <hikaru@nukarumin.local>
2023-05-24 22:42:37 +0900

    不要なもの削除　消し消し

--- script.js
+++ script.js
@@ -9,18 +9,10 @@ function clearTaskTitleInput() {
 
 }
 
-function createTaskListElement(task) {
-    let listElement = document.createElement('li')
-
-    let checkbox = document.createElement('')
-
-}
-
 let onSubmitAddTaskForm = function (event) {
 
     event.preventDefault();
 
-    console.log(event);
     console.log("created new task: " + getTaskTitleInput());
 
     if (getTaskTitleInput() == "") {
@@ -48,38 +40,12 @@ document.getElementById("addTaskForm").addEventListener('submit', onSubmitAddTas
 // jsのDOM制御に書き換えた クール・バージョン
 
 function addTask(task) {
-    taskId++
-
-    let checkBoxId = 'taskDoneCheckbox_' + taskId       // taskDoneCheckbox_1
-
-    // inputタグ （＝チェックボックス自体）
-    let checkBoxHtml = document.createElement('input')  // ただのinputタグを作る
-    checkBoxHtml.setAttribute('type', 'checkbox')       // つくった新しいinputタグの「type」属性に "checkbox" を指定
-    checkBoxHtml.setAttribute('id', checkBoxId)         // つくった新しいinputタグの「id」属性に "checkbox" を指定
-    checkBoxHtml.checked = false                        // つくった新しいinputタグの「checked」属性に true を指定
-
-
-    // やりたいこと
-    // 1. チェックボックスが変更されたことを検知
-    // 2. チェックボックスの状態を取得。
-    // 2. 対応するlabelも取得（制御したいから）
-    // 3. チェックが入ってたら、class属性に"done"を設定
-    // 3. 入ってなかったら、class属性に""を設定
-
     let onChangeCheckBox = function (event) {
-        //console.log(event);
-        // console.log(event.target.checked);
-        // console.log(event.target.nextElementSibling);
-        // console.log(event.target);
-        // console.log(event.target.nextElementSibling.children[1]);
-
         let labelHTML = event.target.nextElementSibling
         let span1stHTML = labelHTML.children[0]
         let span2ndHTML = labelHTML.children[1]
-        let deleteButtonHTML = labelHTML.children[2]
 
         if (event.target.checked) {
-            console.log("チェックされたよ");
             span1stHTML.setAttribute('class', 'done')
             span2ndHTML.setAttribute('class', "")
         } else {
@@ -88,6 +54,30 @@ function addTask(task) {
         }
     }
 
+    let onDeleteButtonClick = function (event) {
+        event.stopPropagation();
+        
+        let deleteListHTML =  event.currentTarget.parentElement.parentElement
+        deleteListHTML.remove()
+    }
+
+    taskId++
+    let checkBoxId = 'taskDoneCheckbox_' + taskId       // taskDoneCheckbox_1
+
+    // inputタグ （＝チェックボックス自体）
+    let checkBoxHtml = document.createElement('input')  // ただのinputタグを作る
+    checkBoxHtml.setAttribute('type', 'checkbox')       // つくった新しいinputタグの「type」属性に "checkbox" を指定
+    checkBoxHtml.setAttribute('id', checkBoxId)         // つくった新しいinputタグの「id」属性に "checkbox" を指定
+    checkBoxHtml.checked = false                        // つくった新しいinputタグの「checked」属性に true を指定
+
+    // やりたいこと
+    // 1. チェックボックスが変更されたことを検知
+    // 2. チェックボックスの状態を取得。
+    // 2. 対応するlabelも取得（制御したいから）
+    // 3. チェックが入ってたら、class属性に"done"を設定
+    // 3. 入ってなかったら、class属性に""を設定
+
+  
     checkBoxHtml.addEventListener("change", onChangeCheckBox)
 
     // labelタグ （＝チェックボックスの隣にある文字）
@@ -105,18 +95,6 @@ function addTask(task) {
     trashIconHTML.setAttribute('class', 'fa-solid fa-trash')
     deleteButtonHTML.appendChild(trashIconHTML)
 
-    let onDeleteButtonClick = function (event) {
-        console.log(typeof (event.currentTarget));
-        console.log(event.currentTarget);
-        console.log(event);
-        console.log(event.currentTarget.parentElement.parentElement);
-
-        event.stopPropagation();
-
-        let deleteListHTML =  event.currentTarget.parentElement.parentElement
-
-        deleteListHTML.remove()
-    }
 
     deleteButtonHTML.addEventListener('click', onDeleteButtonClick)
 
@@ -128,26 +106,22 @@ function addTask(task) {
     checkBoxLabelHtml.setAttribute('for', checkBoxId)       // 作った新しいinputタグの「id」属性に "checkbox" を指定
 
     let listHtml = document.createElement('li')
-    console.log("1. ", listHtml.outerHTML);
     // <li>
     // </li>
 
     listHtml.appendChild(checkBoxHtml)
-    console.log("2. ", listHtml.outerHTML);
     // <li>
     //     <input type="checkbox" id="taskDoneCheckbox_1" checked></input>
     // </li>
 
     listHtml.appendChild(checkBoxLabelHtml)
-    console.log("3. ", listHtml.outerHTML);
     // <li>
     //     <input type="checkbox" id="taskDoneCheckbox_1" checked></input>
     //     <label for="taskDoneCheckbox_1">みそきん買う</label>
-    // </li>
-
-    console.log("added list html:", listHtml)
+    // </li>  
 
     document.getElementById("taskList").appendChild(listHtml)
+    console.log("added list html:", listHtml)
 }
 
 
